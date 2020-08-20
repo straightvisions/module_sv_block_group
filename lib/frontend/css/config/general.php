d7a29c2605b92ac1fa660214cdaedc9acd03753a
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-08-20 20:46:29 +0200

    OPTIMIZE SELECTOR

--- lib/frontend/css/config/general.php
+++ lib/frontend/css/config/general.php
@@ -1,7 +1,7 @@
 <?php
 	$settings = $script->get_parent()->get_settings();
 	echo reset($settings)->build_css(
-		is_admin() ? '.editor-styles-wrapper ul, .editor-styles-wrapper .wp-block-group' : '.sv100_sv_content_wrapper article ul, .sv100_sv_content_wrapper article .wp-block-group',
+		is_admin() ? '.editor-styles-wrapper ul, .editor-styles-wrapper .wp-block-group' : '.sv100_sv_content_wrapper article .wp-block-group',
 		array_merge(
 			$script->get_parent()->get_setting('padding')->get_css_data('padding'),
 			$script->get_parent()->get_setting('margin')->get_css_data(),
