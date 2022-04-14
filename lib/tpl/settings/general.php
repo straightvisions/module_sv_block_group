df02803990018ef2a34264f6ed764a7662939e23
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-04-14 13:04:01 +0200

    Remove legacy settings

--- lib/tpl/settings/general.php
+++ lib/tpl/settings/general.php
@@ -3,12 +3,6 @@
 		<h2><?php _e('General', 'sv100'); ?></h2>
 		<div class="sv_setting_flex">
 			<?php
-				echo $module->get_setting( 'section_dynamic_visibility' )->form();
-				echo $module->get_setting( 'sticky_offset' )->form();
-			?>
-		</div>
-		<div class="sv_setting_flex">
-			<?php
 				echo $module->get_setting( 'margin' )->form();
 				echo $module->get_setting( 'padding' )->form();
 				echo $module->get_setting( 'bg_padding' )->form();
