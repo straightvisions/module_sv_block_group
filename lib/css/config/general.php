90e5fcab545185e7e7818622f7ea7e7a90ffee98
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:29 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,7 +1,7 @@
 <?php
 	// common
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-group' : '.wp-block-group',
+		'.wp-block-group',
 		array_merge(
 			$module->get_setting('padding')->get_css_data('padding'),
 			$module->get_setting('margin')->get_css_data(),
@@ -10,7 +10,7 @@
 	);
 
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-group.has-background' : '.wp-block-group.has-background',
+		'.wp-block-group.has-background',
 		array_merge(
 			$module->get_setting('bg_padding')->get_css_data('padding')
 		)
